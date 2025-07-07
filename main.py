20fd322e9d381c1886872fd3184160b3d4d781c2
Sacrusas <karamshahid28@gmail.com>
2025-07-07 22:10:52 +0500

    Added FPS

--- main.py
+++ main.py
@@ -6,13 +6,16 @@ def main():
     print("Starting Asteroids!")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     BLACK = (0, 0, 0)
+    clock = pygame.time.Clock()
+    dt = 0
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill(BLACK)     # <- fixed: call fill on screen, not standalone
-        pygame.display.flip()  # <- needed to actually show the fill
+        pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
